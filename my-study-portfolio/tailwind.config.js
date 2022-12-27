4e0aa219633054c214ed27ad6aeb2160163804a8
Oskar Høgseth <oskar.hogseth@student.uib.no>
2022-12-27 13:38:11 +0100

    added portfolio

---
+++ my-study-portfolio/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  
+  theme: {
+    extend: {
+      fontFamily:{
+        burtons: "burtons",
+      },
+    },
+  },
+  plugins: [],
+}
